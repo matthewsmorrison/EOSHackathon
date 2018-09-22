db75782e4a3efa3131318568eb800f8c70c15062
Matt <matthew.seamus.morrison@gmail.com>
2018-09-22 17:07:24 +0100

    Initial start on private data smart contract

--- eosio_docker/contracts/notechain/notechain.cpp
+++ eosio_docker/contracts/notechain/notechain.cpp
@@ -1,81 +1,77 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/print.hpp>
 using namespace eosio;
+using namespace std;
 
-// Smart Contract Name: notechain
-// Table struct:
-//   notestruct: multi index table to store the notes
-//     prim_key(uint64): primary key
-//     user(account_name/uint64): account name for the user
-//     note(string): the note message
-//     timestamp(uint64): the store the last update block time
-// Public method:
-//   isnewuser => to check if the given account name has note in table or not
-// Public actions:
-//   update => put the note into the multi-index table and sign by the given account
+class privatesharing : public eosio::contract {
+private:
+  struct datasharing {
+    account_name  requestee;        // the EOS account that has shown willingness to share data
+    uint64_t      data_price;       // the price specified by the owner
 
-// Replace the contract class name when you start your own project
-class notechain : public eosio::contract {
-  private:
-    bool isnewuser( account_name user ) {
-      notetable noteobj(_self, _self);
-      // get object by secordary key
-      auto notes = noteobj.get_index<N(getbyuser)>();
-      auto note = notes.find(user);
+    // primary key
+    account_name primary_key() const { return requestee; }
+  };
 
-      return note == notes.end();
-    }
+  struct request {
+    uint64_t      id;               // the id of the request
+    account_name  requestor;        // the account requesting data
+    account_name  requestee;        // the account of the owner of the data
+    string        url;              // the url where the user needs to send their data
+    bool          filled;           // whether the request has been fulfilled
 
-    /// @abi table
-    struct notestruct {
-      uint64_t      prim_key;  // primary key
-      account_name  user;      // account name for the user
-      std::string   note;      // the note message
-      uint64_t      timestamp; // the store the last update block time
+    // primary key
+    uint64_t primary_key() const { return id; }
 
-      // primary key
-      auto primary_key() const { return prim_key; }
-      // secondary key: user
-      account_name get_by_user() const { return user; }
-    };
+  };
 
-    // create a multi-index table and support secondary key
-    typedef eosio::multi_index< N(notestruct), notestruct,
-      indexed_by< N(getbyuser), const_mem_fun<notestruct, account_name, &notestruct::get_by_user> >
-      > notetable;
+  // local instances of the multi indexes
+  datasharing _datasharing;
+  requestors _requestors;
 
-  public:
-    using contract::contract;
+public:
+  using contract::contract;
 
-    /// @abi action
-    void update( account_name _user, std::string& _note ) {
-      // to sign the action with the given account
-      require_auth( _user );
+  /// @abi action
+  bool signalSharing( account_name _requestee, uint64_t _price ) {
+    require_auth( user );
 
-      notetable obj(_self, _self); // code, scope
-
-      // create new / update note depends whether the user account exist or not
-      if (isnewuser(_user)) {
-        // insert object
-        obj.emplace( _self, [&]( auto& address ) {
-          address.prim_key    = obj.available_primary_key();
-          address.user        = _user;
-          address.note        = _note;
-          address.timestamp   = now();
-        });
-      } else {
-        // get object by secordary key
-        auto notes = obj.get_index<N(getbyuser)>();
-        auto &note = notes.get(_user);
-        // update object
-        obj.modify( note, _self, [&]( auto& address ) {
-          address.note        = _note;
-          address.timestamp   = now();
-        });
+    // Check whether the user has already signalled that they are willing to share data
+    for(auto& item: _datasharing) {
+      if(_datasharing.requestee == _requestee) {
+        return false;
       }
     }
 
+    return true;
+
+
+    // If the user has not already signalled they are willing to share data, add to table
+
+    // _datasharing.emplace(get_self(), [&](auto& p) {
+    //   p.key = _polls.available_primary_key();
+    //   p.pollId = _polls.available_primary_key();
+    //   p.pollName = pollName;
+    //   p.pollStatus = 0;
+    //   p.option = "";
+    //   p.count = 0;
+    // });
+  }
+
+  void acknowledgeRequest( account_name user, account_name requestor ) {
+    require_auth( user );
+
+    // removes the requestor from the dataSharing structure so the front end does not repeat polling
+  }
+
+  bool requestData( account_name from, account_name source, string url ) {
+     // if source is
+
+    return true;
+  }
+
+
+
 };
 
-// specify the contract name, and export a public action: update
-EOSIO_ABI( notechain, (update) )
+EOSIO_ABI( privatesharing, (update) )
